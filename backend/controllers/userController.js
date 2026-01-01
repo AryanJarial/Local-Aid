5d030ad72825f08ab7690e90fca524e2c52b6c22
Aryan Jarial <aryanjarial77@gmail.com>
2026-01-01 18:53:46 +0530

    Add user registration controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,36 @@
+import User from '../models/userModel.js';
+
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400).json({ message: 'User already exists' });
+      return;
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
+    } else {
+      res.status(400).json({ message: 'Invalid user data' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { registerUser };
